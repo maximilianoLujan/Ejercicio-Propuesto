13931ea2110782c26a960f86f887dca48fbd487f
Maximiliano <maxylujan1@outlook.com>
2022-06-02 15:24:37 -0300

    Agregando las carpetas

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+import hamburgerMenu from "./hamburger.js";
+console.log("hola")
+const d = document;
+d.addEventListener("DOMContentLoaded", e=>{
+    hamburgerMenu(".boton",".elmenu",".elmenu a")
+})
