9d4e8e5b4b782cc82ef115f85d42f2da52c7dbb6
Maximiliano <maxylujan1@outlook.com>
2022-06-06 14:06:16 -0300

    Realizando de la seccion 2, la parte de atajos del teclado

--- JS/eventeclado.js
+++ JS/eventeclado.js
@@ -1,16 +1,16 @@
-export default function teclado(pelota){
+export default function teclado(e,pelota){
     const d = document,
         $pelota = d.querySelector(pelota)
-    d.addEventListener("keydown",e=>{
-        console.log(e.key)
-        if (e.key == "ArrowLeft"){
-            $pelota.style.left -= "10px";
-        } else if (e.key == "ArrowUp"){
-            $pelota.style.top -= "10px";
-        } else if (e.key == "ArrowRight"){
-            $pelota.style.left += "10px";
-        } else if (e.key == "ArrowDown"){
-            $pelota.style.top += "10px";
-        }
-    })
+    if (e.key === "ArrowRight"){
+        $pelota.style.left = "40px";
+    }
+    if ((e.code === "KeyA") && (e.shiftKey === true)){
+        alert("Esto es una alerta creada por el atajo del teclado")
+    }
+    if ((e.code === "KeyC") && (e.shiftKey === true)){
+        confirm("Esto es una confirmacion creada por el atajo del teclado")
+    }
+    if ((e.code === "KeyP") && (e.shiftKey === true)){
+        prompt("Esto es un aviso creada por el atajo del teclado")
+    }
 }
--- JS/index.js
+++ JS/index.js
@@ -3,8 +3,10 @@ import hamburgerMenu from "./hamburger.js";
 import reloj from "./reloj.js";
 
 const d = document;
-d.addEventListener("DOMContentLoaded", e=>{
-    hamburgerMenu(".boton",".elmenu",".elmenu a");
-    reloj(".inr",".finr",".hora",".ina",".fina",".audio");
-    teclado(".pelota");
-})
+    d.addEventListener("DOMContentLoaded", e=>{
+        hamburgerMenu(".boton",".elmenu",".elmenu a");
+        reloj(".inr",".finr",".hora",".ina",".fina",".audio");
+    })
+    d.addEventListener("keydown", (e)=>{
+        teclado(e,".pelota");
+    })
