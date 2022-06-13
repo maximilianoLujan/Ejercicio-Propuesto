56ae72d3fb6762c3c90a32b2cbb2c1cf517e0eaa
Maximiliano <maxylujan1@outlook.com>
2022-06-13 19:44:11 -0300

    Terminando ejercicio de geolocalizacion

--- JS/geo.js
+++ JS/geo.js
@@ -14,7 +14,8 @@ export default function descubrirGeo(ul){
         .getCurrentPosition(pos =>{
             $listaDatos.innerHTML = `<li>Precision de ${pos.coords.accuracy} metros</li>
                                     <li>Latitud: ${pos.coords.latitude}</li>
-                                    <li>Longitud: ${pos.coords.longitude}</li>`
+                                    <li>Longitud: ${pos.coords.longitude}</li>
+                                    <a href="https://www.google.com/maps/@${pos.coords.latitude},${pos.coords.longitude},16z" target="_blank" rel="noopener">Visualizar en Google Maps</a>`
         },err=>{
             $listaDatos.innerHTML = `<li>Se ha producido el error ${err.message}</li>`
         },options)
